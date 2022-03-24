c29aa605625d71b857f92faae1cec9d62f6fe110
Balamurugan <balamuruganselvam91803@gmail.com>
2022-03-25 00:40:45 +0530

    add a addition program

---
+++ ConsoleApp3/addition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    /// <summary>
+    /// Main class or entry class for console application
+    /// </summary>
+    internal class addition
+    {
+        //entry point
+      public static void add()
+        {
+            Console.Write("object oriented program in c# \n");
+            Console.Write("----------------------------- \n");
+            int x = 10;
+            int y = 20;
+            int z = x+y;
+            Console.Write("x+y={0},z");
+            Console.ReadKey();
+        }
+
+
+    }
+}
